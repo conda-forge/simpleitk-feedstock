a17c99675c619f35c8bf8a45eda8b3e51cf7595c
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-10-02 21:31:26 +0200

    try slycot workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,23 @@
+if [ -z ${CONDA_BUILD+x} ]; then
+    source /Users/jrodriguez/.local/anaconda/conda-bld/simpleitk_1664738747721/work/build_env_setup.sh
+fi
 #!/bin/bash
 set -euxo pipefail
 
 BUILD_DIR="${SRC_DIR}/build"
-
-mkdir "${BUILD_DIR}"
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" && $target_platform == "osx-arm64" ]]; then
+  # workarounds for https://github.com/conda-forge/simpleitk-feedstock/pull/27#issuecomment-1264707645
+  # refs: 
+  # - https://github.com/scikit-build/scikit-build/issues/589
+  # - https://github.com/conda-forge/slycot-feedstock/issues/34
+  export CMAKE_OSX_ARCHITECTURES="arm64"
+  rm -rf $PREFIX/lib/libpython${PY_VER}.dylib
+  ln -sf $PREFIX/lib/libc++.dylib $PREFIX/lib/libpython${PY_VER}.dylib
+fi
 
 cmake ${CMAKE_ARGS} \
     -G Ninja \
@@ -19,4 +32,4 @@ cmake ${CMAKE_ARGS} \
     "${SRC_DIR}"/Wrapping/Python
 
 cmake --build . --config Release
-${PYTHON} setup.py install
+"${PYTHON}" setup.py install
