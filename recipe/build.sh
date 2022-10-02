a4bd9a0548a88b75e5f5f6e8bfab5f032c9378b8
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2022-10-02 22:52:28 +0200

    try upgrading sitkTargetLinkLibrariesWithDynamicLookup

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,26 +9,21 @@ rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 cd "${BUILD_DIR}"
 
-if [[ $CONDA_BUILD_CROSS_COMPILATION == "1" && $target_platform == "osx-arm64" ]]; then
-  # workarounds for https://github.com/conda-forge/simpleitk-feedstock/pull/27#issuecomment-1264707645
-  # refs: 
-  # - https://github.com/scikit-build/scikit-build/issues/589
-  # - https://github.com/conda-forge/slycot-feedstock/issues/34
-  export CMAKE_OSX_ARCHITECTURES="arm64"
-  rm -rf $PREFIX/lib/libpython${PY_VER}.dylib
-  ln -sf $PREFIX/lib/libc++.dylib $PREFIX/lib/libpython${PY_VER}.dylib
-fi
+OUR_CMAKE_ARGS=""
+OUR_CMAKE_ARGS+="-D CMAKE_BUILD_TYPE:STRING=Release "
+OUR_CMAKE_ARGS+="-D SimpleITK_BUILD_DISTRIBUTE:BOOL=ON "
+OUR_CMAKE_ARGS+="-D SimpleITK_BUILD_STRIP:BOOL=ON "
+OUR_CMAKE_ARGS+="-D BUILD_SHARED_LIBS:BOOL=OFF "
+OUR_CMAKE_ARGS+="-D BUILD_TESTING:BOOL=OFF "
+OUR_CMAKE_ARGS+="-D SimpleITK_PYTHON_USE_VIRTUALENV:BOOL=OFF "
+# if [[ ${CONDA_BUILD_CROSS_COMPILATION:-} == "1" ]]; then
+#   OUR_CMAKE_ARGS+="-D SITK_UNDEFINED_SYMBOLS_ALLOWED:BOOL=ON"
+# fi
 
-cmake ${CMAKE_ARGS} \
+cmake \
+    ${CMAKE_ARGS} \
+    ${OUR_CMAKE_ARGS} \
     -G Ninja \
-    -D "CMAKE_CXX_FLAGS:STRING=-fvisibility=hidden -fvisibility-inlines-hidden ${CXXFLAGS}" \
-    -D "CMAKE_C_FLAGS:STRING=-fvisibility=hidden ${CFLAGS}" \
-    -D CMAKE_BUILD_TYPE:STRING=Release \
-    -D SimpleITK_BUILD_DISTRIBUTE:BOOL=ON \
-    -D SimpleITK_BUILD_STRIP:BOOL=ON \
-    -D BUILD_SHARED_LIBS:BOOL=OFF \
-    -D BUILD_TESTING:BOOL=OFF \
-    -D SimpleITK_PYTHON_USE_VIRTUALENV:BOOL=OFF \
     "${SRC_DIR}"/Wrapping/Python
 
 cmake --build . --config Release
